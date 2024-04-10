800c1008eb708cec50672f7bcbc14cf7002ec5f0
cnilanthan <cnilanthan@tecracer.de>
2024-04-10 14:38:23 +0200

    lambda to handle user queries to llm

--- bedrock-rag/bedrock_rag/bedrock_rag_stack.py
+++ bedrock-rag/bedrock_rag/bedrock_rag_stack.py
@@ -1,6 +1,9 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_lambda as lambda_,
+    aws_iam as iam,
+    Duration
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -17,3 +20,22 @@ class BedrockRagStack(Stack):
         #     self, "BedrockRagQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+
+        llm_handler = lambda_.Function(
+            self, "LLMHandler",
+            runtime=lambda_.Runtime.PYTHON_3_10,
+            handler="llm_handler.llm_handler",  # Assuming index.py with a function named handler
+            code=lambda_.Code.from_asset("bedrock_rag/resources/llm_function"),
+            memory_size=256,
+            timeout=Duration.seconds(60*5),
+        )
+
+        llm_handler.add_to_role_policy(iam.PolicyStatement(
+        effect=iam.Effect.ALLOW,
+        actions=[
+            "bedrock:InvokeModel",
+            "bedrock:InvokeModelWithResponseStream"
+        ],
+        resources=["arn:aws:bedrock:*::foundation-model/*"]
+        
+    ))
