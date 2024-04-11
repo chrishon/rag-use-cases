02ef5cecefe409de8cc8436a2049e77a2eabc990
cnilanthan <cnilanthan@tecracer.de>
2024-04-11 10:24:56 +0200

    network stack

---
+++ bedrock-rag/bedrock_rag/network_stack.py
@@ -0,0 +1,37 @@
+from aws_cdk import (
+    Aws,
+    Stack,
+    aws_ec2 as ec2,
+    aws_ssm as ssm,
+    aws_iam as iam,
+    CfnOutput
+)
+from constructs import Construct
+
+
+class NetworkingStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, stage_id, deploy_sm_domain, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        self.primary_vpc = ec2.Vpc(
+            self,
+            "PrimaryVPC",
+            cidr='192.168.0.0/24',
+            max_azs=3,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="Private",
+                    subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT,
+                    cidr_mask=26,
+                ),
+                
+            ],
+            enable_dns_hostnames=True,
+            enable_dns_support=True,
+            nat_gateways=0,
+        )
+
+        CfnOutput(self, 'VpcId', value=self.primary_vpc.vpc_id)
+        for index, subnet in enumerate(self.primary_vpc.private_subnets):
+            CfnOutput(self, f'PrivateSubnet{index + 1}Id', value=subnet.subnet_id)
+        CfnOutput(self, 'DefaultSecurityGroupID', value=self.primary_vpc.vpc_default_security_group)   
