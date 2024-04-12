f0928ba9d496d9d2db793563f4ddb9222a4eea14
Antonia <aberg@tecracer.de>
2024-04-12 11:03:02 +0200

    networking stack in app.py

--- bedrock-rag/app.py
+++ bedrock-rag/app.py
@@ -4,25 +4,27 @@ import os
 import aws_cdk as cdk
 
 from bedrock_rag.bedrock_rag_stack import BedrockRagStack
+from bedrock_rag.network_stack import NetworkingStack
 
 
 app = cdk.App()
-BedrockRagStack(app, "BedrockRagStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+NetworkingStack(app, "network-stack")
+# BedrockRagStack(app, "BedrockRagStack",
+#     # If you don't specify 'env', this stack will be environment-agnostic.
+#     # Account/Region-dependent features and context lookups will not work,
+#     # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+#     # Uncomment the next line to specialize this stack for the AWS Account
+#     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+#     #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
+#     # Uncomment the next line if you know exactly what Account and Region you
+#     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+#     #env=cdk.Environment(account='123456789012', region='us-east-1'),
 
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+#     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+#     )
 
 app.synth()
