3f0fbc9fc1e3fa8e2933ed3a112adb6cc1cb4546
chrishon <chrishon.nilanthan@gmail.com>
2024-04-16 17:08:36 +0200

    changes in app

--- bedrock-rag/app.py
+++ bedrock-rag/app.py
@@ -6,22 +6,30 @@ import aws_cdk as cdk
 from bedrock_rag.bedrock_rag_stack import BedrockRagStack
 from bedrock_rag.network_stack import NetworkingStack
 from bedrock_rag.vectordb_stack import VectorDBStack
+from bedrock_rag.vectordb_serverless_stack import VectorDBServerlessStack
 from bedrock_rag.s3_knowledgebase_stack import S3Stack
 
 from config.constants import DEFAULT_DEPLOYMENT_REGION, DEFAULT_ACCOUNT
+import boto3
+
+iam = boto3.client("iam")
+user_arn = iam.get_user()["User"]["Arn"]
 
 
 app = cdk.App()
 env = cdk.Environment(account=DEFAULT_ACCOUNT, region=DEFAULT_DEPLOYMENT_REGION)
-# s3Stack = S3Stack(app, "S3Stack", env=env)
-networkStack = NetworkingStack(app, "NetworkStack", env=env)
-vectorDBStack = VectorDBStack(
-    app,
-    "VectorDBStack",
-    vpc=networkStack.primary_vpc,
-    subnets=networkStack.primary_vpc.isolated_subnets,
-    env=env,
+vectorDBServerless = VectorDBServerlessStack(
+    app, "vectorDBServerless", env=env, user_arn=user_arn
 )
+# s3Stack = S3Stack(app, "S3Stack", env=env)
+# networkStack = NetworkingStack(app, "NetworkStack", env=env)
+# vectorDBStack = VectorDBStack(
+#     app,
+#     "VectorDBStack",
+#     vpc=networkStack.primary_vpc,
+#     subnets=networkStack.primary_vpc.isolated_subnets,
+#     env=env,
+# )
 # BedrockRagStack(
 #     app,
 #     "BedrockRagStack",
