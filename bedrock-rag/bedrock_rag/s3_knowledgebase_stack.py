f280cb031d7988e43a31640187f37d470953ae6d
chrishon <chrishon.nilanthan@gmail.com>
2024-04-17 08:53:16 +0200

    knowledge base stack

--- bedrock-rag/bedrock_rag/s3_knowledgebase_stack.py
+++ bedrock-rag/bedrock_rag/s3_knowledgebase_stack.py
@@ -6,6 +6,7 @@ from aws_cdk import (
     aws_lambda as _lambda,
     aws_s3_notifications as notifications,
     Duration,
+    Fn,
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -17,23 +18,31 @@ class KnowledgeBaseStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         ######################################
         ######## S3 Bucket
-
+        opensearch_endpoint = Fn.import_value("VectorDB-OpenSearchEndpoint")
         function = _lambda.Function(
             self,
             "lambda_function",
             runtime=_lambda.Runtime.PYTHON_3_10,
-            handler="ragindex.send_to_opensearch",  # Assuming index.py with a function named handler
+            handler="ragindex.indexer",  # Assuming index.py with a function named handler
             code=_lambda.Code.from_asset("bedrock_rag/resources/rag_index"),
             memory_size=256,
             timeout=Duration.seconds(60 * 5),
+            environment={"opensearch_endpoint": opensearch_endpoint},
         )
+
+        bedrock_access = iam.PolicyStatement(
+            actions=["bedrock:InvokeModel", "bedrock:InvokeModelWithResponseStream"],
+            resources=["arn:aws:bedrock:*::foundation-model/*"],
+        )
+
+        function.add_to_role_policy
 
         bucket = s3.Bucket(
             self,
-            "KnowledgeBaseDocuments",
-            bucket_name="rag-knowledge-base",  # specify a unique bucket name
+            "KnowledgeBaseDocuments",  # specify a unique bucket name
         )
 
+        bucket.grant_read_write(function)
         notification = notifications.LambdaDestination(function)
 
         # assign notification for the s3 event type (ex: OBJECT_CREATED)
